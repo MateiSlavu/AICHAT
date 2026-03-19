b624357a694c602c2703676c372a6a36625fdccc
MateiSlavu <kingonebravo1@gmail.com>
2026-03-19 15:38:15 +0200

    commit eded

---
+++ data.py
@@ -0,0 +1,17 @@
+produse = [
+    {"id": 1, "nume": "Laptop", "pret": 3000},
+    {"id": 2, "nume": "Telefon", "pret": 1500},
+    {"id": 3, "nume": "Vinil", "pret": 200},
+    {"id": 4, "nume": "Căști", "pret": 500},
+    {"id": 5, "nume": "Boxe", "pret": 800},
+    {"id": 6, "nume": "Televizor", "pret": 4000},
+    {"id": 7, "nume": "Frigider", "pret": 3500},
+    {"id": 8, "nume": "Mașină de spălat", "pret": 2500},
+    {"id": 9, "nume": "Cuptor", "pret": 1800},
+    {"id": 10, "nume": "Aspirator", "pret": 1200},
+    {"id": 11, "nume": "Cafetieră", "pret": 300},
+    {"id": 12, "nume": "Mixer", "pret": 400},
+    {"id": 13, "nume": "Fier de călcat", "pret": 350},
+    {"id": 14, "nume": "Televizor OLED", "pret": 6000},
+    {"id": 15, "nume": "Laptop Gaming", "pret": 8000},
+]
--- maiden.py
+++ maiden.py
@@ -1,26 +1,8 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request, Response, render_template
+from data import produse
 
 app = Flask(__name__, template_folder="templates")
 app.json.ensure_ascii = False
-
-
-produse = [
-    {"id": 1, "nume": "Laptop", "pret": 3000},
-    {"id": 2, "nume": "Telefon", "pret": 1500},
-    {"id": 3, "nume": "Vinil", "pret": 200},
-    {"id": 4, "nume": "Căști", "pret": 500},
-    {"id": 5, "nume": "Boxe", "pret": 800},
-    {"id": 6, "nume": "Televizor", "pret": 4000},
-    {"id": 7, "nume": "Frigider", "pret": 3500},
-    {"id": 8, "nume": "Mașină de spălat", "pret": 2500},
-    {"id": 9, "nume": "Cuptor", "pret": 1800},
-    {"id": 10, "nume": "Aspirator", "pret": 1200},
-    {"id": 11, "nume": "Cafetieră", "pret": 300},
-    {"id": 12, "nume": "Mixer", "pret": 400},
-    {"id": 13, "nume": "Fier de călcat", "pret": 350},
-    {"id": 14, "nume": "Televizor OLED", "pret": 6000},
-    {"id": 15, "nume": "Laptop Gaming", "pret": 8000},
-]
 
 
 @app.route('/', methods=['GET'])
@@ -53,12 +35,33 @@ def get_produs(produs_id):
         )
 
     produs = rezultat[0]
-
-    return Response(
-        f"{produs['nume']} - {produs['pret']} lei",
-        content_type="text/plain; charset=utf-8"
-    )
+    return render_template("produs.html", produs=produs)
 
 
+@app.route('/add_produs/', methods=['POST'])
+def add_produs():
+
+    nume = request.json['nume']
+    pret = request.json['pret']
+
+    for p in produse:
+        if p["nume"].lower() == nume.lower():
+            return Response(
+                "Produsul exista deja",
+                status=400,
+                content_type="text/plain; charset=utf-8"
+            )
+
+    new_id = max(p["id"] for p in produse) + 1
+
+    nou_produs = {
+        "id": new_id,
+        "nume": nume,
+        "pret": pret
+    }
+
+    produse.append(nou_produs)
+
+    return jsonify(nou_produs), 201
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
